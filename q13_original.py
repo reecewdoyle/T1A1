3a6ecb5822f58465f58dd7980706767c60a3b75b
reecewdoyle <reece.doyle@icloud.com>
2023-12-04 23:44:41 +1100

    tested 13 solution agaisnt original

--- q13_original.py
+++ q13_original.py
@@ -2,9 +2,6 @@ numbers = [5, 22, 29, 39, 19, 51, 78, 96, 84]
 i = 0
 while (i < len(numbers) - 1) and (numbers[i] < numbers[i+1]):
     i += 1
-
-x = numbers[i] 
+print(i)
 numbers[i] = numbers[i+1]
-numbers[i+1] = x
-print(numbers)
-
+numbers[i+1] = numbers[i]
